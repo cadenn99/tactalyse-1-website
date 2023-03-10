0096f8c356f4f4995dba42a82a4ecba1fe8cf95d
cadenn99 <c.kamminga@student.rug.nl>
2023-03-10 12:03:08 +0100

    .

--- api/src/middleware/authMiddleware.ts
+++ api/src/middleware/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    // Check for token
+    // TODO: Check for token
     next()
 }
