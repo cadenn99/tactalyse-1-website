5734b4247e4bc348c5e3a37b2edd603eb703075d
djairoh <ljxjah@mykolab.com>
2023-03-30 20:12:08 +0200

    working on order page

--- frontend/src/pages/generate.tsx
+++ frontend/src/pages/generate.tsx
@@ -2,30 +2,44 @@ import Background from "@/components/Background";
 import Header from "@/components/Header";
 import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 
+function NoAccess() {
+  return (
+    <>
+      <h1>You don't have access to this page.</h1>
+      <div>Either log in with a privileged account or</div>
+      <Link className="hover:underline" href="/">Go back home?</Link>
+    </>
+  )
+}
 
-//maps to the noPayment endpoint; employee pdf generation
-export default function Generate() {
-  const {data: session } = useSession()
+function Access() {
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  )
+  // TODO: styling, inputformat (name, league, position, excel sheets)
+}
 
-  
+export default function componentSwitcher() {
+  const { data: session} = useSession()
 
   return (
     <div className="toplevel">
       <Head>
-      <title>Tactalyse PDFs</title>
-        <meta name="description" content="Employee only page for football report generation by Tactalyse" />
+        <title>Tactalyse</title>
+        <meta name="description" content="Login page for the Tactalyse PDF generation service" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
       <Background/>
-
-
+      { session?.user.isEmployee && <Access/> || <NoAccess/>}
     </div>
   )
 }
-
 
 export async function getServerSideProps(context) {
   const session = await getSession(context)
--- frontend/src/pages/order.tsx
+++ frontend/src/pages/order.tsx
@@ -4,6 +4,8 @@ import { Report } from "../../typings";
 import { useEffect, useState } from "react"
 import Image from 'next/image'
 import Background from "@/components/Background";
+import { Avatar, Card, CardBody, CardHeader, Typography } from "@material-tailwind/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 
 
@@ -11,18 +13,47 @@ interface Props {
   sampleReports: Report[]
 }
 
+enum Position {
+  Midfielder = "midfielder",
+  Goalkeeper = "goalkeeper",
+}
+
+enum League {
+
+}
+
+interface Inputs {
+  name: string,
+  position: Position,
+  league: League,
+}
+
 export default function order({ sampleReports }: Props) {
   const [report, setReport] = useState<Report | null>(null)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
 
-  useEffect(() => {
-    setReport(
-      sampleReports[Math.floor(Math.random() * sampleReports.length)]
-    )
-  }, [sampleReports])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  
+  const onSubmit: SubmitHandler<Inputs> = async (values) => {
+    setLoading(true)
+
+    setLoading(false)
+  }
+
+  // useEffect(() => {
+  //   setReport(
+  //     sampleReports[Math.floor(Math.random() * sampleReports.length)]
+  //   )
+  // }, [sampleReports])
 
 
   return (
-    <div className="relative flex w-screen h-screen flex-col md:items-center md:justify-center lg:h-[100vh]">
+    <div className="grid grid-cols-2 grid-rows-1 w-screen h-screen flex-col md:items-center md:justify-center lg:h-[100vh]">
       <Head>
         <title>Order</title>
         <meta name="description" content="Orderpage for football report generation by Tactalyse" />
@@ -31,19 +62,33 @@ export default function order({ sampleReports }: Props) {
       </Head>
       <Header/>
       <Background/>
-      
-      <div className="relative items-start py-10"> {/* FIXME: Not happy with the positioning of this one */}
-        <Image
-          width={500}
-          height={200}
-          src={`${report?.graph_path || "/failed_report_loading.png"}`}
-          blurDataURL="/failed_report_loading.png"
-          placeholder="blur"
-          alt="example report"  //FIXME: incorporate this into Report datatype? 
-        />
+
+      <div className="cols-start-1 cols-end-1 grid grid-cols-1 md:grid-cols-2">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+      {/* example cards here */}
       </div>
-      <div className="flex inset-y-0 right-0 w-16 text-black">
-        placeholder Text
+
+      <div className="cols-start-2 cols-end-2 min-w-[50vw]">
+        <form className="relative mt-24 space-y-8 rounded bg-red-100/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+          <input type="text" placeholder="Name" className="input" {...register('name', {required: true})}/> 
+          {/* <input type="text" placeholder="Position" className="input" {...register('position', {required: true})}/>  */}
+          <select className="input" {...register('position', {required: true})} >
+            <option value="midfielder">Midfielder</option>
+            <option value="goalkeeper">Goalkeeper</option>
+            <option value="???">???</option>
+          </select>
+          {/* <input type="text" placeholder="League" className="input" {...register('league', {required: true})}/>  */}
+          <select className="input" {...register('league', {required: true})} >
+            <option value="?">?</option>
+            <option value="??">??</option>
+            <option value="???">???</option>
+          </select>          
+          <button onClick={handleSubmit(onSubmit)} type="submit" className="w-full rounded bg-[#ff2301] py-3 font-semibold">Submit</button>
+        </form>
+        {/* Form input here */}
       </div>
     </div>
   )
