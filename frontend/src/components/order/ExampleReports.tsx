4e2a365cedbdb48ed37890e45eb30abd42b65e2b
cadenn99 <c.kamminga@student.rug.nl>
2023-05-17 13:16:53 +0200

    Fixed report example embed on mobile

--- frontend/src/components/order/ExampleReports.tsx
+++ frontend/src/components/order/ExampleReports.tsx
@@ -7,17 +7,20 @@ function ExampleReports() {
       <h2 className="text-2xl dark:text-white">Example reports</h2>
       <Tabs.Group style="default" className="flex">
         <Tabs.Item title="Player 1">
-          <object data="/report_1.pdf" className="w-full h-[400px]"></object>
+          <embed
+            className="w-full h-[400px]"
+            type="application/pdf"
+            src="/report_1.pdf"
+          />
         </Tabs.Item>
         <Tabs.Item title="Player 2">
-          <object
-            data="/report_1.pdf"
+          <embed
             className="w-full h-[400px]"
-            height={"100%"}
-          ></object>
+            type="application/pdf"
+            src="/report_1.pdf"
+          />
         </Tabs.Item>
         <Tabs.Item title="Player 3">
-          {/* <object data="/report_1.pdf" className="w-full h-[400px]"></object> */}
           <embed
             className="w-full h-[400px]"
             type="application/pdf"
