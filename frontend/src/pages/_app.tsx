82411f018b86575db104db0406053c74dfde306b
cadenn99 <c.kamminga@student.rug.nl>
2023-05-29 13:14:04 +0200

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -62,7 +62,7 @@ export default function App({
     textInput: {
       field: {
         input: {
-          base: "block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-[#FF2301]/80 focus:ring-[#FF2301]/80 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-[#FF2301]/80 dark:focus:ring-[#FF2301]/80 pl-10 rounded-lg p-2.5 text-sm",
+          base: "block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:!border-[#FF2301]/80 focus:!ring-[#FF2301]/80 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:!border-[#FF2301]/80 dark:focus:!ring-[#FF2301]/80 pl-10 rounded-lg p-2.5 text-sm",
         },
       },
     },
